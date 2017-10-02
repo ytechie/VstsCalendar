c83f159b590bba7c799eb3ef6920dbd73e3fb9c0
Jason Young <jason@ytechie.com>
2017-10-02 16:01:27 -0700

    Fix start date label

--- GenerateIcs/vstsCalGen.js
+++ GenerateIcs/vstsCalGen.js
@@ -83,7 +83,7 @@ function cleanRawWorkItems(rawWorkItems) {
 
         wi.title = rawWorkItems[i].fields['System.Title'];
         wi.who = rawWorkItems[i].fields['System.AssignedTo'];
-        wi.start = moment.utc(rawWorkItems[i].fields['TEDCOM.ACTIVITYSTART']).startOf('day').toDate();
+        wi.start = moment.utc(rawWorkItems[i].fields['CSEngineering.ActivityStartDate']).startOf('day').toDate();
         wi.duration = rawWorkItems[i].fields['CSEngineering.ActivityDuration'];
         wi.end = moment.utc(wi.start).add(wi.duration, 'days').toDate();
         wi.url = 'https://' + vstsSiteName + '.visualstudio.com/DefaultCollection/' + encodeURIComponent(vstsProjectName)
